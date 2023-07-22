1473897d3a06f64c196ae4696355ad319bb4bd04
adeyinka.dev <makindeyinkax@gmail.com>
2023-07-22 17:38:45 +0100

    collected staticfiles

--- django_project/settings.py
+++ django_project/settings.py
@@ -121,6 +121,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
+STATICFILES_DIRS = [BASE_DIR / "static"]
+STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_STORAGE = "django.contrib.staticfiles.storage.StaticFilesStorage"
+
 
 # Media
 
