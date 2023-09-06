975c41ce0a4fbc6eb49faa15f4c99133ff5b91e8
adeyinka.dev <makindeyinkax@gmail.com>
2023-09-06 20:11:28 +0100

    switch debug mode to check issues

--- django_project/settings.py
+++ django_project/settings.py
@@ -30,7 +30,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env.bool("DJANGO_DEBUG", default=True)
+DEBUG = env.bool("DJANGO_DEBUG", default=False)
 
 ALLOWED_HOSTS = ["stama.onrender.com", "localhost", "127.0.0.1"]
 
